679ffab0dd975e7aa18ffe3d1085b9bdb2575c49
Ryan Gregoire <ryangregoire286@gmail.com>
2024-12-19 10:40:11 -0500

    Added  Day 1 and 2 of 30 Days of JavaScript

---
+++ JavaScript/Day 1/My First Program/Main.js
@@ -0,0 +1,9 @@
+/*
+    console.log ('Hello World');
+
+    - Prints Hello World to console or Command Line
+*/
+
+console.log("Hello World");
+
+console.log("Hello " + "Ryan James");
---
+++ JavaScript/Day 2/Variables/main.js
@@ -0,0 +1,20 @@
+let message;
+
+message = "Hello World";
+
+console.log(message);
+
+let user = "Ryan",
+  age = 25,
+  message1 = message;
+
+let myName;
+
+let messages;
+myName = "Hello Ryan";
+
+messages = myName;
+
+console.log(myName, messages);
+
+// console.log(user, age, message1);
