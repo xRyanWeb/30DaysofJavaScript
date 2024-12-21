b936cdab8994c40a97f47f737ef9882c67be8992
Ryan James Gregoire <ryangregoire286@gmail.com>
2024-12-21 09:15:23 -0500

    Added Varaibles

--- JavaScript/Day 1/My First Program/Main.js
+++
@@ -1,9 +0,0 @@
-/*
-    console.log ('Hello World');
-
-    - Prints Hello World to console or Command Line
-*/
-
-console.log("Hello World");
-
-console.log("Hello " + "Ryan James");
--- JavaScript/Day 2/Project/Item Divided/weeklyDividedCalc.js
+++
@@ -1,8 +0,0 @@
-const item = (699.0).toFixed(2);
-const weekly = (15.0).toFixed(2);
-const days = (2.0).toFixed(2);
-const hours = (3.0).toFixed(2);
-
-let calc = Math.floor(item / (weekly * days * hours).toFixed(2));
-
-console.log(`It will Take you ${calc} Weeks to Buy Item That is: $${item}`);
